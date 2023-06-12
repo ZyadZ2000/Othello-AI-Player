b863adec6f1178f6ff5d2d5f1694433291a60de0
dina12118 <85133414+dina12118@users.noreply.github.com>
2023-06-12 20:42:34 +0300

    Update Game_Settings.java

--- Othello/Board/Game_Settings.java
+++ Othello/Board/Game_Settings.java
@@ -238,6 +238,133 @@ public class Game_Settings {
 
         return Valid;
     }
+	
+	public static ArrayList<Point> get_Flipped_Points(int[][] board, int player, int ROW, int COL){
+
+        int Modified_Row , Modified_Col ;
+        int Enemy;
+        if(player == PLAYER_ONE){
+            Enemy= PLAYER_TWO;
+        }
+        else{
+            Enemy= PLAYER_ONE;
+        }
+        ArrayList<Point> Reverse_Points = new ArrayList<>();
+
+        for(int i = 0;i<8;i++){
+            ArrayList<Point> temp = new ArrayList<>();
+            if(i == 0){
+                Modified_Row = ROW - 1;
+                Modified_Col = COL;
+                while(Modified_Row > 0 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Row--;
+                }
+                if(Modified_Row >= 0 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 1){
+                Modified_Row = ROW + 1;
+                Modified_Col = COL;
+                while(Modified_Row < 7 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Row++;
+                }
+                if(Modified_Row <= 7 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 2){
+                Modified_Row = ROW;
+                Modified_Col = COL - 1;
+                while(Modified_Col > 0 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Col--;
+                }
+                if(Modified_Col >= 0 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 3){
+                Modified_Row = ROW;
+                Modified_Col = COL + 1;
+
+                while(Modified_Col < 7 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Col++;
+                }
+                if(Modified_Col <= 7 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 4){
+                Modified_Row = ROW - 1;
+                Modified_Col = COL - 1;
+                while(Modified_Row > 0 && Modified_Col > 0 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Row--;
+                    Modified_Col--;
+                }
+                if(Modified_Row >= 0 && Modified_Col >= 0 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 5){
+                Modified_Row = ROW - 1;
+                Modified_Col = COL + 1;
+                while(Modified_Row > 0 && Modified_Col < 7 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Row--;
+                    Modified_Col++;
+                }
+                if(Modified_Row >= 0 && Modified_Col <= 7 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 6){
+                Modified_Row = ROW + 1;
+                Modified_Col = COL - 1;
+                while(Modified_Row < 7 && Modified_Col > 0 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Row++;
+                    Modified_Col--;
+                }
+                if(Modified_Row <= 7 && Modified_Col >= 0 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+            else if(i == 7){
+                Modified_Row = ROW + 1;
+                Modified_Col = COL + 1;
+                while(Modified_Row < 7 && Modified_Col < 7 && board[Modified_Row][Modified_Col] == Enemy){
+                    temp.add(new Point(Modified_Row,Modified_Col));
+                    Modified_Row++;
+                    Modified_Col++;
+                }
+                if(Modified_Row <= 7 && Modified_Col <= 7 && board[Modified_Row][Modified_Col] == player && temp.size() > 0){
+                    Reverse_Points.addAll(temp);
+                }
+            }
+        }
+        return Reverse_Points;
+    }
+
+    public static int[][] update_Board(int[][] board, Point move , int player){
+
+        int[][] UpdatedBoard = new int[BOARD_LENGTH][BOARD_WIDTH];
+        for (int row = 0; row < BOARD_LENGTH; row++) {
+            for (int col = 0; col < BOARD_WIDTH; col++) {
+                UpdatedBoard[row][col] = board[row][col];
+            }
+        }
+        ArrayList<Point> Flipped = Game_Settings.get_Flipped_Points(UpdatedBoard,player,move.x,move.y);
+        UpdatedBoard[move.x][move.y] = player;
+        for(Point pt : Flipped){
+            UpdatedBoard[pt.x][pt.y] = player;
+        }
+        return UpdatedBoard;
+    }
 
 
    
